d7384679dba99d86603f8bdea7625e7d8e21ff34
Jefferson Aggor <aggorjefferson@gmail.com>
2020-07-17 13:13:08 +0000

    final commit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -25,7 +25,11 @@ router.get("/", requireLogin, (req, res) => {
         .limit(3)
         .sort({ _id: -1 })
         .then((book) => {
-          res.render("dashboard/dashboard", { loggedUser: req.user, room, book });
+          res.render("dashboard/dashboard", {
+            loggedUser: req.user,
+            room,
+            book,
+          });
         });
     })
     .catch((err) =>
@@ -103,44 +107,73 @@ router.post(
 // edit room
 router.put(
   "/edit/:_id",
+  requireLogin,
   multerDestination("./room-images/edited").single("image"),
   (req, res) => {
     Room.findOne({ _id: req.params._id }).then((room) => {
-      cloudinary.uploader.upload(
-        req.file.path,
-        eagerOptions,
-        (error, result) => {
-          if (error) {
-            req.flash("error_msg", "Something bad happened. Try again");
-            res.redirect(`/hb/dashboard/edit/${req.params._id}`);
-          } else {
-            (room.room = req.body.room),
-              (room.price = req.body.price),
-              (room.description = req.body.description),
-              (room.porch = validateCheckbox(req.body.porch, "on")),
-              (room.wardrobe = validateCheckbox(req.body.wardrobe, "on")),
-              (room.ac = validateCheckbox(req.body.ac, "on")),
-              (room.monoBeds = validateCheckbox(req.body.monoBeds, "on")),
-              (room.bathroomInside = validateCheckbox(req.body.bathroom, "on")),
-              (room.kitchen = validateCheckbox(req.body.kitchen, "on")),
-              (room.mainRoomImage = result.eager[0].secure_url);
+      if (req.file) {
+        cloudinary.uploader.upload(
+          req.file.path,
+          eagerOptions,
+          (error, result) => {
+            if (error) {
+              req.flash("error_msg", "Something bad happened. Try again");
+              res.redirect(`/hb/dashboard/edit/${req.params._id}`);
+            } else {
+              (room.room = req.body.room),
+                (room.price = req.body.price),
+                (room.description = req.body.description),
+                (room.porch = validateCheckbox(req.body.porch, "on")),
+                (room.wardrobe = validateCheckbox(req.body.wardrobe, "on")),
+                (room.ac = validateCheckbox(req.body.ac, "on")),
+                (room.monoBeds = validateCheckbox(req.body.monoBeds, "on")),
+                (room.bathroomInside = validateCheckbox(
+                  req.body.bathroom,
+                  "on"
+                )),
+                (room.kitchen = validateCheckbox(req.body.kitchen, "on")),
+                (room.mainRoomImage = result.eager[0].secure_url);
 
-            room
-              .save()
-              .then((editted) => {
-                req.flash(
-                  "success_msg",
-                  "Room has been updated. Back to dashboard"
-                );
-                res.redirect(`/hb/dashboard/edit/${req.params._id}`);
-              })
-              .catch((err) => {
-                req.flash("error_msg", "Something bad happened. Try again");
-                res.redirect(`/hb/dashboard/edit/${req.params._id}`);
-              });
+              room
+                .save()
+                .then((editted) => {
+                  req.flash(
+                    "success_msg",
+                    "Room has been updated. Back to dashboard"
+                  );
+                  res.redirect(`/hb/dashboard/edit/${req.params._id}`);
+                })
+                .catch((err) => {
+                  req.flash("error_msg", "Something bad happened. Try again");
+                  res.redirect(`/hb/dashboard/edit/${req.params._id}`);
+                });
+            }
           }
-        }
-      );
+        );
+      } else {
+        (room.room = req.body.room),
+          (room.price = req.body.price),
+          (room.description = req.body.description),
+          (room.porch = validateCheckbox(req.body.porch, "on")),
+          (room.wardrobe = validateCheckbox(req.body.wardrobe, "on")),
+          (room.ac = validateCheckbox(req.body.ac, "on")),
+          (room.monoBeds = validateCheckbox(req.body.monoBeds, "on")),
+          (room.bathroomInside = validateCheckbox(req.body.bathroom, "on")),
+          (room.kitchen = validateCheckbox(req.body.kitchen, "on")),
+          room
+            .save()
+            .then((editted) => {
+              req.flash(
+                "success_msg",
+                "Room has been updated. Back to dashboard"
+              );
+              res.redirect(`/hb/dashboard/edit/${req.params._id}`);
+            })
+            .catch((err) => {
+              req.flash("error_msg", "Something bad happened. Try again");
+              res.redirect(`/hb/dashboard/edit/${req.params._id}`);
+            });
+      }
     });
   }
 );
